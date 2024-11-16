cb7d452193f43a3dea5b19abdd879fe3d816fd21
Khushwant <khushwantsingh638@gmail.com>
2024-11-16 07:12:02 +0530

    supahot commit as I added a fire animation

--- app/card/page.jsx
+++ app/card/page.jsx
@@ -16,9 +16,12 @@ import {
   DrawerHeader,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 const page = () => {
-  const Ingredients = [];
+  const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows
+`;
+
   const [heart, setHeart] = useState("/icons/heart-regular.svg");
 
   const handleHeart = () => {
